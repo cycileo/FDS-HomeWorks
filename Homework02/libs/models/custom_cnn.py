d18bca61ba0efe81eec5fb384d5db7d4dbdd971e
cycileo <roccileonardo0@gmail.com>
2024-11-27 12:40:01 +0100

    Done: section 5

--- Homework02/libs/models/custom_cnn.py
+++ Homework02/libs/models/custom_cnn.py
@@ -1,12 +1,37 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
+from torch.nn import functional as F    
+
 
 class CustomCNN(nn.Module):
     def __init__(self):
         super(CustomCNN, self).__init__()
         ##############################
-        ###     YOUR CODE HERE     ###
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 20, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(20),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Conv2d(20, 35, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(35),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(35, 60, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(60),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Dropout(0.20)
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(60 * 4 * 4, 512),
+            nn.ReLU(),
+            nn.Dropout(0.45),
+            nn.Linear(512, 10)
+        )
         ##############################  
         # Define the convolutional layers
         
@@ -14,7 +39,9 @@ class CustomCNN(nn.Module):
 
     def forward(self, x):
         ##############################
-        ###     YOUR CODE HERE     ###
+        x = self.features(x)
+        x = x.view(-1, 60 * 4 * 4)
+        x = self.classifier(x)
         ##############################  
         # Define the forward pass
         return x
